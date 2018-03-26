54ba65c1f1296aec091fc4838e3b19e4f4d92f1b
vishal2082 <vishalbhardwaj070@gmail.com>
2018-03-26 12:32:31 +0530

    adding test cases

---
+++ src/test/java/com/codebind/AppTest.java
@@ -0,0 +1,14 @@
+package com.codebind;
+
+//import static org.junit.Assert.*;
+
+//import org.junit.Test;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tests1 is executed ...");
+
+}
+}
