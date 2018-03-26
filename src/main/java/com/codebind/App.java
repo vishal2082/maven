0578fddeb2367f1b4400f47583a6e499b1ce0ac7
vishal2082 <vishalbhardwaj070@gmail.com>
2018-03-26 10:30:51 +0530

    push code

---
+++ src/main/java/com/codebind/App.java
@@ -0,0 +1,12 @@
+package com.codebind;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My first Maven project");
+
+	}
+
+}
